924356b821e2d42014a26971477c8d2c7075238b
Pierre Spring <pierre.spring@caillou.ch>
2021-04-21 15:24:29 +0200

    Add eslint and prettier conifg packages

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Monorepo Linting Playgound
+
+This repo is a playground to figure out how to set up linting in a Monorepo.
+
+## Usage
+
+```
+npm install
+```
---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+
+  overrides: [
+    {
+      files: ['**/*.js'],
+      parserOptions: {
+        ecmaVersion: 8,
+      },
+      extends: ['eslint:recommended'],
+    },
+    {
+      files: ['**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        'no-console': 'error',
+        '@typescript-eslint/no-var-requires': 2,
+        '@typescript-eslint/array-type': [
+          'error',
+          {
+            default: 'array',
+            readonly: 'array',
+          },
+        ],
+      },
+    },
+  ],
+};
